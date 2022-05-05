5d145fc15983ea1080982268c7005f7d26eb91a3
megu <gabrieltan2121@gmail.com>
2022-05-05 14:33:30 +0800

    test.py

---
+++ test.py
@@ -0,0 +1,47 @@
+import psutil
+import platform
+from datetime import datetime
+
+def get_size(bytes, suffix="B"):
+    """
+    Scale bytes to its proper format
+    e.g:
+        1253656 => '1.20MB'
+        1253656678 => '1.17GB'
+    """
+    factor = 1024
+    for unit in ["", "K", "M", "G", "T", "P"]:
+        if bytes < factor:
+            return f"{bytes:.2f}{unit}{suffix}"
+        bytes /= factor
+
+# Print system information
+print("="*40, "System Information", "="*40)
+uname = platform.uname()
+print(f"System: {uname.system}")
+print(f"Node Name: {uname.node}")
+print(f"Release: {uname.release}")
+print(f"Version: {uname.version}")
+print(f"Machine: {uname.machine}")
+print(f"Processor: {uname.processor}")
+
+# Network information
+print("="*40, "Network Information", "="*40)
+# get all network interfaces (virtual and physical)
+if_addrs = psutil.net_if_addrs()
+for interface_name, interface_addresses in if_addrs.items():
+    for address in interface_addresses:
+        print(f"=== Interface: {interface_name} ===")
+        if str(address.family) == 'AddressFamily.AF_INET':
+            print(f"  IP Address: {address.address}")
+            print(f"  Netmask: {address.netmask}")
+            print(f"  Broadcast IP: {address.broadcast}")
+        elif str(address.family) == 'AddressFamily.AF_PACKET':
+            print(f"  MAC Address: {address.address}")
+            print(f"  Netmask: {address.netmask}")
+            print(f"  Broadcast MAC: {address.broadcast}")
+
+# get IO statistics since boot
+net_io = psutil.net_io_counters()
+print(f"Total Bytes Sent: {get_size(net_io.bytes_sent)}")
+print(f"Total Bytes Received: {get_size(net_io.bytes_recv)}")
