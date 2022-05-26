97442999af088f1744bc3c654170eeaf79d87d1c
megu <gabrieltan2121@gmail.com>
2022-05-26 16:59:12 +0800

    cleanup

--- Anti Keylogger/antikeylogger.py
+++ Anti Keylogger/antikeylogger.py
@@ -5,25 +5,26 @@ import vt
 import time
 from sys import stdout
 
-##Names of software which should be removed
+#Names of software which should be removed
+###Add a box which shows the blacklist and allow to add
 blacklist = ['keylogger', 'Keylogger']
 
-##Process class to retrieve process name and process PID
+#Process class to retrieve process name and process PID
 class Process(object):
     def __init__(self, process_info):
         self.name = process_info[0]
         self.pid = process_info[1]
 
-##Function to remove keylogger
+#Function to remove keylogger
 def removeKeylogger(pid):
     stdout.write("Killing process")
     ##9 because it represents termination https://en.wikipedia.org/wiki/Signal_(IPC)
     os.kill(int(pid), 9)
 
+###Button here to run this function
 def retrieveProcessList():
     processList = []
     keyloggerDetected = 0
-
     ##Function to use cmd and use "tasklist" to list all running processes
     processes = Popen(['tasklist'],shell = False, stdout = PIPE)
     ##Remove unwanted lines
@@ -31,7 +32,7 @@ def retrieveProcessList():
     processes.stdout.readline()
     processes.stdout.readline()
 
-    ##Loop to look at all processes individually
+    #Loop to look at all processes individually
     for line in processes.stdout:
         ##Put processes into an array
         processInfo = line.decode('utf-8').replace("b'","").split()
@@ -42,7 +43,7 @@ def retrieveProcessList():
         if len(processInfo) == 6:
             processList.append(Process(processInfo))
     
-    ##Loop through processList to look at each process
+    #Loop through processList to look at each process
     for process in processList:
         ##Loop through blacklisted terms and see if it matches the process
         for blacklisted in blacklist:
@@ -56,14 +57,14 @@ def retrieveProcessList():
         print("No keylogger was detected.")
     exit()
 
-##Virustotal API key
+#Virustotal API key (Stay Hidden)
 client = vt.Client("9227fccdca71a13c63c2cffba56b893341dc44b73b6e567aa8197d4d5ca0c0d3")
-##Need to implement file input here
+###Need to implement file input here
 file = client.get_object("xx")
-##Need to implement button to run this code
+###Need to implement button to run this code
 with open("logfile", "rb") as f:
     analysis = client.scan_file(file, wait_for_completion=True)
 
-##Start
+#Start
 if __name__ == '__main__':
     retrieveProcessList()
